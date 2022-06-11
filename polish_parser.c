40b4de325a8c31113cb8d59747ac1daaeca81d5e
madrajib <madhabsharma94@gmail.com>
2022-06-11 19:45:08 +0530

    lisp: Add Q expression syntax to the grammer

--- polish_parser.c
+++ polish_parser.c
@@ -292,6 +292,7 @@ int main(int argc, char** argv) {
   mpc_parser_t* Number   = mpc_new("number");
   mpc_parser_t* Symbol = mpc_new("symbol");
   mpc_parser_t* Sexpr = mpc_new("sexpr");
+  mpc_parser_t* Qexpr = mpc_new("qexpr");
   mpc_parser_t* Expr     = mpc_new("expr");
   mpc_parser_t* Lispy    = mpc_new("lispy");
   
@@ -300,10 +301,11 @@ int main(int argc, char** argv) {
       number   : /-?[0-9]+/ ;                             \
       symbol   : '+' | '-' | '*' | '/' | \"min\" | \"max\";                  \
       sexpr    : '(' <expr>* ')' ;                        \
-      expr     : <number> | <symbol> | <sexpr> ;  \
+      qexpr    : '{' <expr>* '}' ;                        \
+      expr     : <number> | <symbol> | <sexpr> | <qexpr> ;  \
       lispy    : /^/ <expr>* /$/ ;             \
     ",
-    Number, Symbol, Sexpr, Expr, Lispy);
+    Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
   
   puts("Lispy Version 0.0.0.0.2");
   puts("Press Ctrl+c to Exit\n");
@@ -333,7 +335,7 @@ int main(int argc, char** argv) {
   }
   
   /* Undefine and delete our parsers */
-  mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Lispy);
+  mpc_cleanup(4, Number, Symbol, Sexpr ,Qexpr , Expr, Lispy);
   
   return 0;
 }
