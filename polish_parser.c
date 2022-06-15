ef02ade664bad1807eeb8e8b15f8070e812871e4
madrajib <madhabsharma94@gmail.com>
2022-06-15 07:44:18 +0530

    lisp: Add variable regex in mpca_lang

--- polish_parser.c
+++ polish_parser.c
@@ -394,8 +394,7 @@ int main(int argc, char** argv) {
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
       number   : /-?[0-9]+/ ;                             \
-      symbol   : '+' | '-' | '*' | '/' | \"min\" | \"max\" \
-                 |  \"head\" | \"tail\" | \"list\" | \"eval\";                  \
+      symbol   :  /[a-zA-Z0-9_+\\-*\\/\\\\=<>!&]+/ ;      \
       sexpr    : '(' <expr>* ')' ;                        \
       qexpr    : '{' <expr>* '}' ;                        \
       expr     : <number> | <symbol> | <sexpr> | <qexpr> ;  \
